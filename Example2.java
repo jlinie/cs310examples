515bf482b2cc2ef5b983784fcb7afa02bb17ed5f
Ceylin <113205139+jlinie@users.noreply.github.com>
2022-10-31 16:31:23 +0300

    Update Example2.java

--- Example2.java
+++ Example2.java
@@ -1,6 +1,24 @@
 import java.util.Scanner;
 
 public class Example2{
-	
+		public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter year");
+		int curr_year = input.nextInt();
+		
+		if(curr_year % 4 == 0 ) {
+			if(curr_year %100==0) {
+				if(curr_year %400==0) {
+					System.out.println("It is a leap year!");
+				}else {
+					System.out.println("It's not a leap year.");
+				}
+			}else {
+				System.out.println("It's not a leap year.");
+			}
+		}else {
+			System.out.println("It's not a leap year.");
+		}	
+	}
 	
 }
